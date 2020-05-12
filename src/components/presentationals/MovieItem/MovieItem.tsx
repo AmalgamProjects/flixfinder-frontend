ed4d3fcc327d8c3dc4c9f9e6213ca54d04df6a6c
jswiton <jacek.switon@gmail.com>
2020-05-13 00:08:38 +0200

    Added some movies mocked data and covers

--- src/components/presentationals/MovieItem/MovieItem.tsx
+++ src/components/presentationals/MovieItem/MovieItem.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { IMovie } from '../../../types/movie';
+import Styles from './MovieItem.module.css';
 
 interface IOwnProps {
   movie: IMovie;
 }
 
 const MovieItem: React.FC<IOwnProps> = ({ movie }) => (
-<div>{movie.title}</div>
+  <div className={Styles.wrapper}>
+    <img className={Styles.cover} src={movie.imageUrl} alt={movie.title} />
+    {movie.title}
+  </div>
 );
 
 export default MovieItem;
