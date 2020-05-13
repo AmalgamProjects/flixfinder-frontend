652f1060df66954a104d04a70349e7f96e90d09d
jswiton <jacek.switon@gmail.com>
2020-05-13 23:47:43 +0200

    Implemented Heading component

--- src/components/presentationals/Heading/Heading.tsx
+++ src/components/presentationals/Heading/Heading.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import Styles from './Heading.module.scss';
 
-const Heading: React.FC = () => (
-  <div>Heading</div>
+interface IOwnProps {
+  title: string;
+}
+
+const Heading: React.FC<IOwnProps> = ({ title }) => (
+  <h1 className={Styles.heading}>{title}</h1>
 );
 
 export default Heading;
