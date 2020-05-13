7d24add3a5855dd45a400aec24d50a755e0ecde1
jswiton <jacek.switon@gmail.com>
2020-05-13 23:48:49 +0200

    Added reccomended pages

---
+++ src/components/pages/ReccomendedTvShows/ReccomendedTvShows.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Layout } from '../../containers';
+
+const ReccomendedTvShows: React.FC = () => (
+  <Layout>
+    ReccomendedTvShows
+  </Layout>
+);
+
+export default ReccomendedTvShows;
