b21722501b19161dbddff57ef95ee14ebe392cde
Matt <mattchmielowski1@gmail.com>
2020-05-13 23:39:37 +0200

    Added Slider to Dashboard

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { Movies, Layout } from '../../containers';
+import Slider from '../../containers/Slider/Slider';
 
 const Dashboard: React.FC = () => (
   <Layout>
+    <Slider />
     <Movies />
   </Layout>
 );
