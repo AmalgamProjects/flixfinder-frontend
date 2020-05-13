8e7e1d73604f6feba6c5d1e149a13aba1101aafe
jswiton <jacek.switon@gmail.com>
2020-05-13 18:50:01 +0200

    Implemented 404

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,16 +1,17 @@
-import React, { Fragment } from 'react';
-import { Route } from 'react-router-dom';
-import { Dashboard, Login, Onboarding, MyProfile, Watchlist, Movie } from '../components/pages';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Dashboard, Login, Onboarding, MyProfile, Watchlist, Movie, NotFountRoute } from '../components/pages';
 
 const Routes = () => (
-  <Fragment>
+  <Switch>
     <Route path="/" exact component={Dashboard} />
     <Route path="/login" exact component={Login} />
     <Route path="/onboarding" exact component={Onboarding} />
     <Route path="/profile" exact component={MyProfile} />
     <Route path="/watchlist" exact component={Watchlist} />
     <Route path="/movie/:id" component={Movie} />
-  </Fragment>
+    <Route component={NotFountRoute} />
+  </Switch>
 );
 
 export default Routes;
