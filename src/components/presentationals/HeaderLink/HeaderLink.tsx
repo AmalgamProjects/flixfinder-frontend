8c2e1ecaf7f2924d3400c0deca7b34027d18820f
Matt <mattchmielowski1@gmail.com>
2020-05-14 00:04:08 +0200

    HeaderLink active

--- src/components/presentationals/HeaderLink/HeaderLink.tsx
+++ src/components/presentationals/HeaderLink/HeaderLink.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import Styles from './HeaderLink.module.scss';
+import { withRouter, RouteComponentProps } from 'react-router';
+import cx from 'classnames';
 
 interface IOwnProps {
   label: string;
@@ -8,13 +10,25 @@ interface IOwnProps {
   url: string;
 }
 
-const HeaderLink: React.FC<IOwnProps> = ({ label, icon, url }) => (
-  <div className={Styles.wrapper}>
-    <Link to={url}>
-      <span className={Styles.icon}>{icon}</span>
-      <span className={Styles.label}>{label}</span>
-    </Link>
-  </div>
-);
+class HeaderLink extends React.Component<IOwnProps & RouteComponentProps<{ id: string }>> {
+  // const HeaderLink: React.FC<IOwnProps> = ({ label, icon, url }) => (
+  render() {
+    const { label, icon, url, location } = this.props;
+    const { pathname } = location;
+    const wrapper = cx({
+      [Styles.wrapper]: true,
+      [Styles.wrapperCurrent]: pathname === url
+    });
 
-export default HeaderLink;
+    return (
+      <div className={wrapper}>
+        <Link to={url}>
+          <span className={Styles.icon}>{icon}</span>
+          <span className={Styles.label}>{label}</span>
+        </Link>
+      </div>
+    )
+  }
+}
+
+export default withRouter(HeaderLink);
