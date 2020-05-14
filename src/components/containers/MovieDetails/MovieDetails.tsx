1c1d2fc987a45c3d98e1ad6902535c95e8ef6075
Matt <mattchmielowski1@gmail.com>
2020-05-14 22:25:48 +0200

    updated movie item hardcoded data

--- src/components/containers/MovieDetails/MovieDetails.tsx
+++ src/components/containers/MovieDetails/MovieDetails.tsx
@@ -94,12 +94,12 @@ class MovieDetails extends Component<Props> {
           <Container>
             <ContentWithHeader heading="More Like This">
               <Row>
-                <Col><MovieItem movie={{ id: '1', title: 'Awesome movie (1)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '2', title: 'Awesome movie (2)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '3', title: 'Awesome movie (3)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '4', title: 'Awesome movie (4)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '5', title: 'Awesome movie (5)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '6', title: 'Awesome movie (6)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                <Col><MovieItem movie={ { id: '1', user: '1', title: 'Awesome movie (1)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                <Col><MovieItem movie={ { id: '1', user: '2', title: 'Awesome movie (2)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                <Col><MovieItem movie={ { id: '1', user: '3', title: 'Awesome movie (3)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                <Col><MovieItem movie={ { id: '1', user: '4', title: 'Awesome movie (4)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                <Col><MovieItem movie={ { id: '1', user: '5', title: 'Awesome movie (5)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                <Col><MovieItem movie={ { id: '1', user: '6', title: 'Awesome movie (6)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
               </Row>
             </ContentWithHeader>
           </Container>
