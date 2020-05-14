0c593806d0da5482a2af5545d68d6ba22bb562a9
Matt <mattchmielowski1@gmail.com>
2020-05-14 22:51:21 +0200

    MovieDtails content styles

--- src/components/containers/MovieDetails/MovieDetails.tsx
+++ src/components/containers/MovieDetails/MovieDetails.tsx
@@ -47,62 +47,58 @@ class MovieDetails extends Component<Props> {
         </div>
 
         <div className={Styles.wrapper}>
-
           <Container>
-            <ContentWithHeader heading="Storyline">
-              <p className={Styles.text}>
-                Quentin Tarantino's Once Upon a Time... in Hollywood visits 1969 Los Angeles, where everything is changing, as TV star Rick Dalton (Leonardo DiCaprio) and his longtime stunt double Cliff Booth (Brad Pitt) make their way around an industry they hardly recognize anymore. The ninth film from the writer-director features a large ensemble cast and multiple storylines in a tribute to the final moments of Hollywood's golden age.
-              </p>
-            </ContentWithHeader>
-          </Container>
+            <div className={Styles.content}>
 
-          <Container>
-            <ContentWithHeader heading="Cast & Crew">
-              <div className={Styles.cast}>
+              <ContentWithHeader heading="Storyline">
+                <p className={Styles.text}>
+                  Quentin Tarantino's Once Upon a Time... in Hollywood visits 1969 Los Angeles, where everything is changing, as TV star Rick Dalton (Leonardo DiCaprio) and his longtime stunt double Cliff Booth (Brad Pitt) make their way around an industry they hardly recognize anymore. The ninth film from the writer-director features a large ensemble cast and multiple storylines in a tribute to the final moments of Hollywood's golden age.
+                </p>
+              </ContentWithHeader>
+
+              <ContentWithHeader heading="Cast & Crew">
+                <div className={Styles.cast}>
+                  <Row>
+                    <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
+                    <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
+                    <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
+                    <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
+                    <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
+                    <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
+                  </Row>
+                </div>
+              </ContentWithHeader>
+
+              <ContentWithHeader heading="Reviews">
+                <div className={Styles.review}>
+                  <Review
+                    rating="8.9/10"
+                    review="The firstst 45 minutes fasten your seatbelts because classic Tarantino kicks in."
+                    user="carlvan1977 - 27 July 2019"
+                  />
+                </div>
+                <div className={Styles.review}>
+                  <Review
+                    rating="8.9/10"
+                    review="The fir last 45 minutes fasten your seatbelts because classic Tarantino kicks in."
+                    user="carlvan1977 - 27 July 2019"
+                  />
+                </div>
+              </ContentWithHeader>
+
+              <ContentWithHeader heading="More Like This">
                 <Row>
-                  <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
-                  <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
-                  <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
-                  <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
-                  <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
-                  <Col><Person image="https://via.placeholder.com/100" title="Tony Hawk" subtitle="Andrew Reynolds" /></Col>
+                  <Col><MovieItem movie={{ id: '1', user: '1', title: 'Awesome movie (1)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                  <Col><MovieItem movie={{ id: '1', user: '2', title: 'Awesome movie (2)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                  <Col><MovieItem movie={{ id: '1', user: '3', title: 'Awesome movie (3)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                  <Col><MovieItem movie={{ id: '1', user: '4', title: 'Awesome movie (4)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                  <Col><MovieItem movie={{ id: '1', user: '5', title: 'Awesome movie (5)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
+                  <Col><MovieItem movie={{ id: '1', user: '6', title: 'Awesome movie (6)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
                 </Row>
-              </div>
-            </ContentWithHeader>
-          </Container>
+              </ContentWithHeader>
 
-          <Container>
-            <ContentWithHeader heading="Reviews">
-              <div className={Styles.review}>
-                <Review
-                  rating="8.9/10"
-                  review="The first 2 hours is only for lovers of everything late 60s retro, great cinematography and outstanding acting. The last 45 minutes fasten your seatbelts because classic Tarantino kicks in."
-                  user="carlvan1977 - 27 July 2019"
-                />
-              </div>
-              <div className={Styles.review}>
-                <Review
-                  rating="8.9/10"
-                  review="The first 2 hours is only for lovers of everything late 60s retro, great cinematography and outstanding acting. The last 45 minutes fasten your seatbelts because classic Tarantino kicks in."
-                  user="carlvan1977 - 27 July 2019"
-                />
-              </div>
-            </ContentWithHeader>
+            </div>
           </Container>
-
-          <Container>
-            <ContentWithHeader heading="More Like This">
-              <Row>
-                <Col><MovieItem movie={{ id: '1', user: '1', title: 'Awesome movie (1)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '1', user: '2', title: 'Awesome movie (2)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '1', user: '3', title: 'Awesome movie (3)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '1', user: '4', title: 'Awesome movie (4)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '1', user: '5', title: 'Awesome movie (5)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-                <Col><MovieItem movie={{ id: '1', user: '6', title: 'Awesome movie (6)', imageUrl: 'https://images.fineartamerica.com/images-medium-large-5/no051-my-mad-max-minimal-movie-poster-chungkong-art.jpg' }} /></Col>
-              </Row>
-            </ContentWithHeader>
-          </Container>
-
         </div>
       </Fragment>
     );
