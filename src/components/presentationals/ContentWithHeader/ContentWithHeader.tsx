c9d2e6dff5edcb3be3d44e5d44a9332f5fb3b759
Matt <mattchmielowski1@gmail.com>
2020-05-14 22:20:11 +0200

    ContentWithHeader

---
+++ src/components/presentationals/ContentWithHeader/ContentWithHeader.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Styles from './ContentWithHeader.module.scss';
+import { Heading } from '..';
+
+interface IOwnProps {
+  heading: string;
+}
+
+const ContentWithHeader: React.FC<IOwnProps> = ({ heading, children }) => {
+  return (
+    <div style={{ width: '100%' }}>
+      <Heading size="small" title={heading} />
+      <div className={Styles.children}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ContentWithHeader;
