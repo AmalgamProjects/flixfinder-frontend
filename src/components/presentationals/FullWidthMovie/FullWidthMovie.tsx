529b5c6ae90fb0527b0ce829e7da1d2f2ec1dae4
Matt <mattchmielowski1@gmail.com>
2020-05-15 21:52:22 +0200

    FullWidthMovie

--- src/components/presentationals/FullWidthMovie/FullWidthMovie.tsx
+++ src/components/presentationals/FullWidthMovie/FullWidthMovie.tsx
@@ -1,86 +1,87 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import cx from 'classnames';
 import Styles from './FullWidthMovie.module.scss';
 import { Container } from '../../containers';
-import { MovieDetails, Image, TileButton } from '..';
-import movieTestPoster from '../../../assets/images/movie-poster-test.jpg';
+import { MovieDetails, Image, TileButton, Spinner } from '..';
 import { heart, check } from '../../../assets/icons';
+import { IMovie } from '../../../types/movie';
 
 interface IOwnProps {
   isSingleMovie?: boolean;
-  title: string;
-  background: string;
-  details: string;
-
-  director?: string;
-  starring?: string;
-  writer?: string;
-
+  movie: IMovie;
+  isLoading: boolean;
   onAddToWatchlist?: () => void;
   onMarkAsWatched?: () => void;
 }
 
-const FullWidthMovie: React.FC<IOwnProps> = ({
-  director,
-  starring,
-  writer,
-  isSingleMovie,
-  details,
-  background,
-  title,
-  onAddToWatchlist,
-  onMarkAsWatched,
-}) => {
-  const wrapperClassNames = cx({
-    [Styles.wrapper]: true,
-    [Styles.wrapperSingleMovie]: isSingleMovie,
-  });
+class FullWidthMovie extends Component<IOwnProps> {
+  render() {
+    const { onAddToWatchlist, onMarkAsWatched, isSingleMovie, movie, isLoading } = this.props;
+    const { title, image_url, poster_url, averageRating, directors, endYear, genres, moviedb, primaryTitle, principals, rapid, runtimeMinutes, startYear, titleType, summary, backdrop_url, numVotes, tastedb, wikipedia_url, writers, youtube_url } = movie;
 
-  const innerWrapperClassNames = cx({
-    [Styles.innerWrapper]: true,
-    [Styles.innerWrapperSingleMovie]: isSingleMovie,
-  });
+    const wrapperClassNames = cx({
+      [Styles.wrapper]: true,
+      [Styles.wrapperSingleMovie]: isSingleMovie,
+    });
 
-  const contentClassNames = cx({
-    [Styles.content]: true,
-    [Styles.contentSingleMovie]: isSingleMovie,
-  });
+    const innerWrapperClassNames = cx({
+      [Styles.innerWrapper]: true,
+      [Styles.innerWrapperSingleMovie]: isSingleMovie,
+    });
 
-  const detailsClassNames = cx({
-    [Styles.details]: true,
-    [Styles.detailsSingleMovie]: isSingleMovie,
-  });
+    const contentClassNames = cx({
+      [Styles.content]: true,
+      [Styles.contentSingleMovie]: isSingleMovie,
+    });
 
-  return (
-    <div className={wrapperClassNames} style={{ backgroundImage: `url(${background})` }}>
-      <div className={innerWrapperClassNames}>
-        <Container>
-          <div className={contentClassNames}>
-            <div className={Styles.poster}>
-              <Image alt="poster" url={movieTestPoster} />
+    const detailsClassNames = cx({
+      [Styles.details]: true,
+      [Styles.detailsSingleMovie]: isSingleMovie,
+    });
+
+    const getMovieDetails = () => {
+      const formattedGenres = genres && genres.join(', ');
+
+      return `${runtimeMinutes && `${runtimeMinutes}min |`} ${formattedGenres && `${formattedGenres} |`} ${startYear && `${startYear}`}`;
+    }
+
+    return (
+      <div className={wrapperClassNames} style={{ backgroundImage: `url(${image_url})` }}>
+        {isLoading &&
+          <div className={Styles.loader}>
+            <Spinner />
+          </div>}
+        <div className={innerWrapperClassNames}>
+          <Container>
+            <div className={contentClassNames}>
+              {poster_url && <div className={Styles.poster}>
+                <Image alt="poster" url={poster_url} />
+              </div>}
+              <div className={detailsClassNames}>
+                {onAddToWatchlist && onMarkAsWatched && (
+                  <div className={Styles.buttons}>
+                    <TileButton expanded onClick={onAddToWatchlist} label="Add to watchlist" icon={heart} />
+                    <TileButton expanded onClick={onMarkAsWatched} label="Marked as watched" icon={check} />
+                  </div>
+                )}
+                <MovieDetails
+                  title={primaryTitle}
+                  details={getMovieDetails()}
+                  isSingleMovie={isSingleMovie}
+                  // director={'directors'}
+                  // starring={'starring'}
+                  // writer={'writer'}
+                  rating={averageRating}
+                  summary={summary}
+                />
+                <div />
+              </div>
             </div>
-            <div className={detailsClassNames}>
-              {onAddToWatchlist && onMarkAsWatched && (
-                <div className={Styles.buttons}>
-                  <TileButton expanded onClick={onAddToWatchlist} label="Add to watchlist" icon={heart} />
-                  <TileButton expanded onClick={onMarkAsWatched} label="Marked as watched" icon={check} />
-                </div>
-              )}
-              <MovieDetails
-                title={title}
-                details={details}
-                isSingleMovie={isSingleMovie}
-                director={director}
-                starring={starring}
-                writer={writer}
-              />
-              <div />
-            </div>
-          </div>
-        </Container>
+          </Container>
+        </div>
       </div>
-    </div >
-  )
+    )
+  }
 };
 
 export default FullWidthMovie;
