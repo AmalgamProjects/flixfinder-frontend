1ac2faaf24d6333ceca974d3d512b491a164c0ba
jswiton <jacek.switon@gmail.com>
2020-05-15 18:20:48 +0200

    Search API call

---
+++ src/api/search.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import settings from '../settings';
+import { getAuthData } from '../helpers/auth';
+import { ISearchResponse } from '../types/search';
+
+export interface ISearchParams {
+  search: string;
+}
+
+export interface IFetchSearch {
+  (params: ISearchParams): Promise<ISearchResponse | null>;
+}
+
+
+export const search = (params: ISearchParams) => {
+  const baseURL = settings.apiUrl;
+  const accessToken = getAuthData()?.accessToken;
+
+  return axios
+    .request<ISearchResponse>(
+      {
+        baseURL,
+        method: 'GET',
+        params,
+        headers: {
+          ...accessToken && { Authorization: `Bearer ${accessToken}` },
+        },
+        url: '/title/',
+        // cancelToken: apiInstance.source.token,
+      },
+    )
+    .then(res => res.data)
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.error('error: ', error);
+      return null;
+    });
+};
