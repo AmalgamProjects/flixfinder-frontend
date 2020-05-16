1e2bd51819246e3522a1f1387974c154827cc2d0
jswiton <jacek.switon@gmail.com>
2020-05-16 15:47:09 +0200

    Fixed scroll behaviour

--- src/components/presentationals/ScrollToTopOnMount/ScrollToTopOnMount.tsx
+++ src/components/presentationals/ScrollToTopOnMount/ScrollToTopOnMount.tsx
@@ -1,11 +1,29 @@
-import { useEffect } from 'react';
+import { Component } from 'react';
+import { withRouter, RouteComponentProps } from 'react-router';
 
-function ScrollToTopOnMount() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  });
+interface IOwnProps {}
+type Props = IOwnProps & RouteComponentProps;
 
-  return null;
+class ScrollToTopOnMount extends Component<Props> {
+  // eslint-disable-next-line class-methods-use-this
+  componentDidMount() {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 200);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const { location } = this.props;
+
+    if (location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return null;
+  }
 }
 
-export default ScrollToTopOnMount;
+export default withRouter(ScrollToTopOnMount);
