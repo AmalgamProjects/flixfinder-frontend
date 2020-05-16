7e0d8e740728d74829420f468db2e4abe0576f5e
jswiton <jacek.switon@gmail.com>
2020-05-16 12:08:07 +0200

    Split recommended to movies and shows

--- src/components/pages/RecommendedTvShows/RecommendedTvShows.tsx
+++ src/components/pages/RecommendedTvShows/RecommendedTvShows.tsx
@@ -1,10 +1,40 @@
-import * as React from 'react';
-import { Layout, Recommended } from '../../containers';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { loadData } from '../../../redux/userData/actions';
+import { IRootState } from '../../../types/redux';
+import { MoviesSectionList } from '../../presentationals';
+import { IUserDataState } from '../../../redux/userData/reducer';
+import { Layout } from '../../containers';
 
-const RecommendedTvShows: React.FC = () => (
-  <Layout>
-    <Recommended title="Recommended TV Shows" />
-  </Layout>
-);
+interface IOwnProps {}
+interface IConnectedProps { state: IUserDataState; }
+interface IConnectedDispatchProps { onLoadData: typeof loadData; }
+type Props = IOwnProps & IConnectedProps & IConnectedDispatchProps;
 
-export default RecommendedTvShows;
+class RecommendedTvShows extends Component<Props> {
+  componentDidMount() {
+    const { onLoadData } = this.props;
+    onLoadData();
+  }
+
+  render() {
+    const { recommended_tv } = this.props.state;
+
+    return (
+      <Layout>
+        {/* eslint-disable-next-line camelcase */}
+        <MoviesSectionList movies={recommended_tv} title="Recommended TV Shows" />
+      </Layout>
+    );
+  }
+}
+
+export default connect<IConnectedProps, IConnectedDispatchProps, IOwnProps, IRootState>(
+  (state: IRootState) => ({
+    state: state.UserData,
+  }),
+  (dispatch: Dispatch) => ({
+    onLoadData: () => dispatch(loadData()),
+  }),
+)(RecommendedTvShows);
